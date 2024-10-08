7f2c0b4753040853baf3e82b69b0a2e9f32e4330
Bibek Thapa <bibek.thapa619@gmail.com>
2024-10-08 12:24:37 +0545

    Fix pagination button size

--- src/components/pagination/Pagination.vue
+++ src/components/pagination/Pagination.vue
@@ -8,21 +8,10 @@
       <button
         @click="prevPage"
         :disabled="pagination.current_page === 1"
-        class="px-4 py-2 bg-indigo-500 text-white rounded-lg hover:bg-indigo-600 flex items-center"
+        class="px-3 py-2 bg-indigo-500 text-white rounded-lg hover:bg-indigo-600 flex items-center"
         v-if="pagination.current_page > 1"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5 mr-1"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M12.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 111.414 1.414L9.414 10l3.293 3.293a1 1 0 010 1.414z"
-            clip-rule="evenodd"
-          />
-        </svg>
+        <
       </button>
 
       <button
@@ -30,12 +19,12 @@
         :class="
           pagination.current_page === 1 ? 'bg-indigo-700' : 'bg-indigo-500'
         "
-        class="px-4 py-2 text-white rounded-lg hover:bg-indigo-600"
+        class="px-3 py-2 text-white rounded-lg hover:bg-indigo-600"
       >
         1
       </button>
 
-      <span v-if="pagination.current_page > 3" class="px-4 py-2 text-gray-500"
+      <span v-if="pagination.current_page > 3" class="px-3 py-2 text-gray-500"
         >...</span
       >
 
@@ -48,14 +37,14 @@
           'bg-indigo-500': pagination.current_page !== page,
           'text-white rounded-lg hover:bg-indigo-600': true,
         }"
-        class="px-4 py-2"
+        class="px-3 py-2"
       >
         {{ page }}
       </button>
 
       <span
         v-if="pagination.current_page < pagination.last_page - 2"
-        class="px-4 py-2 text-gray-500"
+        class="px-3 py-2 text-gray-500"
         >...</span
       >
 
@@ -66,7 +55,7 @@
             ? 'bg-indigo-700'
             : 'bg-indigo-500'
         "
-        class="px-4 py-2 text-white rounded-lg hover:bg-indigo-600"
+        class="px-3 py-2 text-white rounded-lg hover:bg-indigo-600"
       >
         {{ pagination.last_page }}
       </button>
@@ -74,21 +63,10 @@
       <button
         @click="nextPage"
         :disabled="pagination.current_page === pagination.last_page"
-        class="px-4 py-2 bg-indigo-500 text-white rounded-lg hover:bg-indigo-600 flex items-center"
+        class="px-3 py-2 bg-indigo-500 text-white rounded-lg hover:bg-indigo-600 flex items-center"
         v-if="pagination.current_page < pagination.last_page"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5 ml-1"
-          viewBox="0 0 20 20"
-          fill="currentColor"
         >
-          <path
-            fill-rule="evenodd"
-            d="M7.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L10.586 10 7.293 6.707a1 1 0 010-1.414z"
-            clip-rule="evenodd"
-          />
-        </svg>
       </button>
     </div>
   </div>
